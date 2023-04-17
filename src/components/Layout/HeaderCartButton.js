f8c8d646fb9fcb4c45845f8a991631584f35d4b0
suryansh2206 <suryansh804.sg@gmail.com>
2023-04-17 21:06:39 +0530

    Initial commit(Added header and cart button)

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,16 @@
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCardButton.module.css";
+
+const HeaderCartButton = (props) => {
+  return (
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>0</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
